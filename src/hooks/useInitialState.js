3bb6f4caca7ac5e97d2fb6102bf728f3693c8850
Sugheiry Alcala <sugheiry@gmail.com>
2022-10-05 16:21:26 -0500

    Eliminacion de articulos del carrito de compras

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -13,10 +13,17 @@ const useInitialState = () => {
 			cart: [...state.cart, payload]
 		});
 	};
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: state.cart.filter(items => items.id !== payload.id),
+		});
+	}
 
 	return {
 		state,
-		addToCart
+		addToCart,
+		removeFromCart
 	}
 }
 
